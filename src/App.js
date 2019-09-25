0727a26d0279d48f3fb2078bbb316b37162d78d5
Toniddarden1 <Toniddarden@gmail.com>
2019-09-25 00:15:47 -0400

    changing the file system

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./App.css";
-import Data from './Components.js/Data.js';
+// import Title from "./components/Title"; // <h1> Atsronaunt of the day </h1>
+// import Header from "./Components/Header"; // json data:  Title and Date 
+import Data from './Components.js/Data.js'; // jsona data: explanation 
+// import Image from "./Components/Image"; // json data: url
+
 
 function App() {
   return (
